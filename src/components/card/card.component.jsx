0c7dc2aae9d1948f5bc94dcd1f4697ce88df8cc7
Ignacio Mazuran <ignaciomazuran12@gmail.com>
2021-03-05 10:34:23 -0300

    imgs

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.style.css';
 
 export const Card = props => (
     <div className='card-container'>
-        <img alt="producto" src={`${props.producto.imag}?&size=180x180`}></img>
+        <img alt="producto" src={`public/assets/producto${props.producto.id}.jgp?&size=180x180`}></img>
         <h2>{props.producto.nombre}</h2>
         <p>{props.producto.precio}</p>
     </div>
